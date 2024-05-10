09fbf3eb5db5183e693fb3425b3caa9703d2f3e4
gbd850 <mmyszaki@gmail.com>
2024-05-10 18:00:42 +0200

    Add repository annotation to interfaces

--- src/main/java/com/test/sii/repository/CurrencyRepository.java
+++ src/main/java/com/test/sii/repository/CurrencyRepository.java
@@ -2,9 +2,11 @@ package com.test.sii.repository;
 
 import com.test.sii.model.Currency;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface CurrencyRepository extends JpaRepository<Currency, Integer> {
     Optional<Currency> findByCurrency(String currency);
 }
--- src/main/java/com/test/sii/repository/ProductRepository.java
+++ src/main/java/com/test/sii/repository/ProductRepository.java
@@ -2,6 +2,8 @@ package com.test.sii.repository;
 
 import com.test.sii.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 }
