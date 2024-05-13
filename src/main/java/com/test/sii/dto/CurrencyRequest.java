9ff057be3c5e802632955ff25503200578214a74
gbd850 <mmyszaki@gmail.com>
2024-05-13 16:27:23 +0200

    Delete unused DTOs

--- src/main/java/com/test/sii/dto/CurrencyRequest.java
+++
@@ -1,5 +0,0 @@
-package com.test.sii.dto;
-
-public record CurrencyRequest(
-        String currency
-) {}
--- src/main/java/com/test/sii/dto/CurrencyResponse.java
+++
@@ -1,7 +0,0 @@
-package com.test.sii.dto;
-
-public record CurrencyResponse(
-        Integer id,
-        String currency
-) {
-}
